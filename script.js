61efc77face8d87e826a7fec8e4b7c16ad47d88b
kittykettt <kitketykr@gmail.com>
2024-12-05 10:56:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("search-btn").addEventListener("click", function () {
+    const searchValue = document.getElementById("search-bar").value;
+    alert(`Você pesquisou por: ${searchValue}`);
+  });
+  
